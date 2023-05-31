2afc5998d1a4687339cd82bc535b079567a51947
hayderab <haider.1999@outlook.com>
2023-05-31 21:55:20 +0100

    add mongodb connection, created collections

--- QuestionnaireAppLibirary/DataAccess/Dbconnection.cs
+++ QuestionnaireAppLibirary/DataAccess/Dbconnection.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
-namespace QuestionnaireAppLibirary.DataAccess
+namespace QuestionnaireAppLibirary.DataAccess;
+
+public class Dbconnection
 {
-    public class Dbconnection
+
+    private readonly IConfiguration _config;
+    private readonly IMongoDatabase _db;            //internally useable
+    private string _connectionId = "MongoDB";
+
+
+    public string DbName { get; private set; }
+
+    public string CategoryCollectionName { get; private set; } = "catergories";
+
+    public string StatusCollectionName { get; private set; } = "status";
+
+    public string UserCollectionName { get; private set; } = "user";
+
+    public string QuestionnaireCollectionName { get; private set; } = "questionnaire";
+
+
+
+
+    public MongoClient Client { get; private set; } // client connects to database then we connect to collection/tables
+    // connection to tables
+    public IMongoCollection<CategoryModel> CategoryCollection { get; private set; }
+    public IMongoCollection<StatusModal> StatusCollection { get; private set; }
+    public IMongoCollection<QuestionnaireModel> QuestionnaireCollection { get; private set; }
+    public IMongoCollection<UserModal> UserCollection { get; private set; }
+
+
+    // constructor
+    public Dbconnection(IConfiguration config)
     {
+        _config = config;
+        Client = new MongoClient(_config.GetConnectionString(_connectionId));
+        DbName = _config[key: "DatabaseName"];
+        _db = Client.GetDatabase(DbName);
+
+
+        // only initiate database once and reuse collection connetion and enhance performance.
+        CategoryCollection = _db.GetCollection<CategoryModel>(CategoryCollectionName);
+        StatusCollection = _db.GetCollection<StatusModal>(StatusCollectionName);
+        QuestionnaireCollection = _db.GetCollection<QuestionnaireModel>(QuestionnaireCollectionName);
+        UserCollection = _db.GetCollection<UserModal>(UserCollectionName);
     }
+
+
+
+
+
 }
--- QuestionnaireAppLibirary/Models/UserModal.cs
+++ QuestionnaireAppLibirary/Models/UserModal.cs
@@ -8,13 +8,13 @@ namespace QuestionnaireAppLibirary.Models
 
          public string ObjectIndentifier { get; set; }
 
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
 
-        public string? Lastname { get; set; }
+        public string Lastname { get; set; }
 
-        public string? DisplayName { get; set; }
+        public string DisplayName { get; set; }
 
-        public string? Email { get; set; }
+        public string Email { get; set; }
 
         public List<BasicQuestionnaireModel> AuthoredQuestionnaire { get; set; } = new();
         public List<BasicQuestionnaireModel> VotedOnQuestionnaire { get; set; } = new();
