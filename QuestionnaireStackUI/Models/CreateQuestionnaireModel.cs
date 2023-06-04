eda19b690554b5f1f631c229e417b8bef2b9fb48
hayderab <haider.1999@outlook.com>
2023-06-04 22:14:54 +0100

    functionalities for creating questionnaire

---
+++ QuestionnaireStackUI/Models/CreateQuestionnaireModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuestionnaireStackUI.Models
+{
+    public class CreateQuestionnaireModel
+    {
+        [Required]
+        [MaxLength(75)]
+        public string QuestionnaireTitle { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [Display(Name = "Category")]
+        public string CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string QuestionnaireDesc { get; set; }
+
+        public List<QuestionModel> Questions { get; set; } = new List<QuestionModel>();
+
+
+    }
+}
